f3bff8ab96785b6db6ebb268316b8af43dee74da
shashank1406 <satu5050@gmail.com>
2021-11-08 19:13:07 +0530

    nov 8 assignment

---
+++ radium 1/src/util/helper.js
@@ -0,0 +1,12 @@
+function printDate(){
+    console.log("today is 08/november/2021")
+}
+function printMonth(){
+    console.log(" the month is november")
+}
+function batchInfo(){
+    console.log("function up week fourth day one we are starting a nodejs today")
+}
+module.exports.printDate = printDate;
+module.exports.printMonth = printMonth;
+module.exports.batchInfo = batchInfo;
