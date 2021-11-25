bee047afebe8d147ad9388019b30f48652895a8f
shashank1406 <satu5050@gmail.com>
2021-11-25 17:38:31 +0530

    23/24 nov assignment

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -6,22 +6,34 @@ const jwt = require('jsonwebtoken')
 
 
 const createUser = async function (req, res) {
+    try {
     var data = req.body
     let savedData = await userModel.create(data)
     res.send({ data: savedData })
+    } catch (error) {
+    res.status(500).send({ status: false, message: error.message });
+    }
 }
-   
+
+
 const login = async function (req, res) {
-    let check = await userModel.findOne({ password: req.body.password, email: req.body.email, isDeleted: false })
+   
+   try{
+        let check = await userModel.findOne({ password: req.body.password, email: req.body.email, isDeleted: false })
     if (check) {
         let payload = { _id: check._id }
-        let token = jwt.sign(payload, 'shashank')
+        let token = jwt.sign(payload , 'shashank')
         res.send({ "msg": "true", "data": check, "tokendetail": token })
     } else {
         res.send({ "msg": "false" })
     }
+} catch (error) {
+    res.status(500).send({ status: false, message: error.message });
+    }
 }
+
 const dataById = async function (req, res) {
+   try{
     let userId = req.params.userId
     let tokenUserid = req.body.validToken._id
     if (tokenUserid == userId) {
@@ -35,34 +47,35 @@ const dataById = async function (req, res) {
     } else {
         res.send({ status: false, msg: "not authorized user" })
     }
-
-
+} catch (error) {
+    res.status(500).send({ status: false, message: error.message });
+    }
 }
 
+
+
+
 const updateName = async function (req, res) {
+    try{
     let userId = req.params.userId
     let tokenUserid = req.body.validToken._id
     if (tokenUserid == userId) {
         let check = await userModel.findById(userId)
         let newName = req.body.name
-    if (check) {
-        await userModel.findOneAndUpdate({ _id: userId, name: newName })
-        res.send({ status: "updated", data:{check} })
-    } else {
-        res.send({ "msg": "#error-response-structure" })
-    }
+        if (check) {
+            await userModel.findOneAndUpdate({ _id: userId, name: newName })
+            res.send({ status: "updated", data: { check } })
+        } else {
+            res.send({ "msg": "#error-response-structure" })
+        }
 
     } else {
         res.send({ status: false, msg: "not authorized user" })
     }
+} catch (error) {
+    res.status(500).send({ status: false, message: error.message });
+    }
 }
-
-
-
-
-
-
-
 
 module.exports.createUser = createUser
 module.exports.login = login
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,14 +1,18 @@
 
 const jwt = require('jsonwebtoken')
 
-let tokenCheck = function(req,res,next){
-    let token =req.headers['x-auth-token']
-    let validToken = jwt.verify(token,'shashank')
-    if(validToken){
-       req.body.validToken = validToken
+let tokenCheck = function (req, res, next) {
+    try {
+    let token = req.headers['x-auth-token']
+    let validToken = jwt.verify(token, 'shashank')
+    if (validToken) {
+        req.body.validToken = validToken
         next()
-    }else{
-        res.send({status:false,msg:"invalid token"})
+    } else {
+        res.send({ status: false, msg: "invalid token" })
     }
+} catch (error) {
+    res.status(500).send({ status: false, message: error.message });
+  }
 }
-module.exports.tokenCheck=tokenCheck
+module.exports.tokenCheck = tokenCheck
